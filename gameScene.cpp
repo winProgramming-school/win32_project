8d32198c8a5d73a324b01c25813e40a2227f162d
halin130 <halin130@naver.com>
2021-06-03 20:16:36 +0900

    gamescene 제작..

---
+++ gameScene.cpp
@@ -0,0 +1,49 @@
+#include "stdafx.h"
+#include "gameScene.h"
+//memdc에 그려주는 역할, frame
+WGameFramework framework;
+
+gameScene::~gameScene()
+{
+
+}
+
+void gameScene::init()
+{
+
+
+}
+void gameScene::processKey(UINT iMessage, WPARAM wParam, LPARAM lParam)
+{
+    switch (iMessage)
+    {
+    case WM_KEYDOWN:
+    {
+        switch (wParam) {
+        case VK_RETURN: //enter 키누르면 게임시작
+            scene* scene = framework.curScene;
+            //framework.curScene = new gameScene;
+            framework.curScene->init();
+            delete scene;
+            break;
+        }
+    }
+    break;
+    }
+}
+
+//애니메이션 있으면 여기서 업데이트
+void gameScene::Update()
+{
+    
+}
+//메뉴에서 그려줄 리소스들 그리기
+void gameScene::Render(HDC hdc)
+{
+    
+}
+
+//void menuScene::Release() {
+//
+//
+//}
--- gameScene.h
+++ gameScene.h
@@ -1,1 +1,18 @@
 #pragma once
+#include "scene.h"
+
+class gameScene : public scene {
+public:
+
+public:
+	virtual ~gameScene() override;
+
+	virtual void init() override;
+
+	virtual void processKey(UINT iMessage, WPARAM wParam, LPARAM lParam) override;
+
+	virtual void Update() override;
+
+	virtual void Render(HDC hdc) override;
+
+};
